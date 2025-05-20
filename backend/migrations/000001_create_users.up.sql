a99317f363e062afaf47b8259c0bef769f0fbffe
bryryann <186111776+bryryann@users.noreply.github.com>
2025-05-20 18:10:19 -0300

    build: Add create_users migration
    - Add migration related commands to Makefile

---
+++ backend/migrations/000001_create_users.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
