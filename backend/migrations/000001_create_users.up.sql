f8aebb37eb53b93389bc83b5606ef8648be12577
bryryann <186111776+bryryann@users.noreply.github.com>
2025-05-21 15:20:28 -0300

    fix: Change users table 'name' field to 'username'

--- backend/migrations/000001_create_users.up.sql
+++ backend/migrations/000001_create_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    name text NOT NULL,
+    username text NOT NULL,
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
     version integer NOT NULL DEFAULT 1
