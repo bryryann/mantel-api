08a37422efeed779e62e00695032cd7227fd5c30
bryryann <186111776+bryryann@users.noreply.github.com>
2025-06-03 22:49:07 -0300

    feat: Toggle between log in and sign up forms in authentication form

--- frontend/src/components/auth/RegisterForm/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm/RegisterForm.tsx
@@ -1,17 +1,20 @@
 import useInput from '@/hooks/useInput';
 import styles from '../auth.module.css';
 
-const LoginForm = () => {
+const RegisterForm = () => {
     const username = useInput('text');
+    const email = useInput('email');
     const password = useInput('password');
 
     return (
         <form className={styles.form}>
             <input placeholder='Username' {...username.bind} />
+            <input placeholder='Email' {...email.bind} />
             <input placeholder='Password' {...password.bind} />
-            <button type='submit'>Log In</button>
+            <button type='submit'>Create Account</button>
         </form>
     );
 };
 
-export default LoginForm;
+export default RegisterForm;
+
--- frontend/src/pages/AuthorizationScreen/AuthorizationScreen.tsx
+++ frontend/src/pages/AuthorizationScreen/AuthorizationScreen.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import LoginForm from '@/components/auth/LoginForm';
+import RegisterForm from '@/components/auth/RegisterForm';
 import logoColor from '@/assets/images/logo-color.png';
 import monogramColor from '@/assets/images/monogram-color.png';
 import styles from './AuthorizationScreen.module.css';
 
 const AuthorizationScreen = () => {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [isLogin, setIsLogin] = useState(true);
 
     useEffect(() => {
         const handleResize = () => {
@@ -19,6 +21,12 @@ const AuthorizationScreen = () => {
         };
     }, []);
 
+    const toggleForm = () => { setIsLogin(prev => !prev); };
+
+    const CurrentForm = isLogin ? LoginForm : RegisterForm;
+    const toggleMessage = isLogin ? "Don't have an account yet?" : "Already have an account?";
+    const toggleButton = isLogin ? "Sign up" : "Log in";
+
     return (
         <div className={styles.container}>
             <header>
@@ -31,13 +39,14 @@ const AuthorizationScreen = () => {
                 <h3>Sign in or register a new account</h3>
             </header>
             <div>
-                <LoginForm />
+                {<CurrentForm />}
                 <div className={styles.toggle}>
-                    Don't have an account yet?
+                    {toggleMessage}
                     <button
                         type='button'
+                        onClick={toggleForm}
                     >
-                        Sign up.
+                        {toggleButton}
                     </button>
                 </div>
             </div>
