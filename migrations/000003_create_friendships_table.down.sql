424cb86b33b0c6a9c4693f8c4ae812b44fe56014
bryryann <186111776+bryryann@users.noreply.github.com>
2025-07-04 20:09:40 -0300

    db: Create 'friendships' table to database

---
+++ migrations/000003_create_friendships_table.down.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER IF EXISTS trigger_update_friendship_version ON friendships;
+
+DROP FUNCTION IF EXISTS update_friendship_version();
+
+DROP TABLE IF EXISTS friendships;
+
---
+++ migrations/000003_create_friendships_table.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS friendships (
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    friend_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'blocked')) DEFAULT 'pending',
+    version INTEGER NOT NULL DEFAULT 1,
+
+    PRIMARY KEY (user_id, friend_id),
+    CHECK (user_id < friend_id)
+);
+
+CREATE OR REPLACE FUNCTION update_friendship_version()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at := NOW();
+    NEW.version := OLD.version + 1;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_friendship_version
+BEFORE UPDATE ON friendships
+FOR EACH ROW
+EXECUTE FUNCTION update_friendship_version();
