ff3c268df2a1143ff6401cbdb1439b3b83942e1f
bryryann <186111776+bryryann@users.noreply.github.com>
2025-07-19 16:27:59 -0300

    BREAKING: Change 'friendship' table for REST compliance. Break previous versions of API

--- migrations/000003_create_friendships_table.up.sql
+++ migrations/000003_create_friendships_table.up.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS friendships (
-    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    friend_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sender_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    receiver_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'blocked')) DEFAULT 'pending',
     version INTEGER NOT NULL DEFAULT 1,
 
-    PRIMARY KEY (user_id, friend_id),
-    CHECK (user_id <> friend_id)
+    CHECK (sender_id <> receiver_id)
+    UNIQUE (LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id))
 );
 
 CREATE OR REPLACE FUNCTION update_friendship_version()
