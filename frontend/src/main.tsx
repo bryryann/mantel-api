4bc7db69b10afb3b5026e9de86c614d6e4a8e4c1
bryryann <186111776+bryryann@users.noreply.github.com>
2025-08-17 18:52:01 -0300

    Set up basic Redux store and Auth-related slice.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import { Provider } from 'react-redux';
+import { store } from '@/app/store';
 
 const root = createRoot(document.getElementById('root')!)
 
 function renderApplication() {
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
 }
